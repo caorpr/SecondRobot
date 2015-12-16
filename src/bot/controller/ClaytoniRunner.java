ef1676fd07684fd0df777be4c5e2eb0ad08ab4db
caorpr <caorpr@gmail.com>
2015-12-16 12:12:15 -0700

    Made a Runner & Controller
    REALLY simple

---
+++ src/bot/controller/ClaytoniRunner.java
@@ -0,0 +1,10 @@
+package bot.controller;
+
+public class ClaytoniRunner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/bot/model/RobotController.java
@@ -0,0 +1,9 @@
+package bot.model;
+
+public class RobotController 
+{
+	public void start()
+	{
+		
+	}
+}
