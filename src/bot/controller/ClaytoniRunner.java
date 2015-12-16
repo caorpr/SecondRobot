44384d1aee794b219d487de56c55761bf8f73750
caorpr <caorpr@gmail.com>
2015-12-16 13:45:31 -0700

    Fixed the Controller

--- src/bot/controller/ClaytoniRunner.java
+++ src/bot/controller/ClaytoniRunner.java
@@ -1,9 +1,13 @@
 package bot.controller;
 
+import bot.model.RobotController;
+
 public class ClaytoniRunner {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		RobotController myRobotController = new RobotController();
+		myRobotController.start();
 
 	}
 
